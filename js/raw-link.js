0b6113bf024286d46f8e6a4afe069ba42a962170
alanfoster <alan@alanfoster.me>
2021-03-20 18:38:52 +0000

    Add initial raw link support with netlify functions

---
+++ js/raw-link.js
@@ -0,0 +1,44 @@
+/**
+ * Generates a RawLink for the reverse shell generator. If the user hasn't changed
+ * the default generated shell command, the generated URL will contain the original
+ * parameters to generate the required command on demand.
+ *
+ * Otherwise a unique URL is created which inlined the current user provided command.
+ */
+const RawLink = {
+    generate: (rsg) => {
+        const commandSelector = rsg.uiElements[rsg.commandType].command;
+        const currentCommandElement = document.querySelector(commandSelector);
+        const defaultGeneratedCommand = rsg.generateReverseShellCommand();
+        const isUserProvidedCommand = currentCommandElement.innerHTML != RawLink.escapeHTML(defaultGeneratedCommand);
+
+        if (isUserProvidedCommand) {
+            return RawLink.withCustomValue(currentCommandElement.innerText)
+
+        }
+        return RawLink.withDefaultPayload(rsg);
+    },
+
+    escapeHTML(html) {
+        var element = document.createElement('div');
+        element.innerHTML = html;
+        return element.innerHTML;
+    },
+
+    withDefaultPayload: (rsg) => {
+        const name = rsg.selectedValues[rsg.commandType];
+        const queryParams = new URLSearchParams();
+        queryParams.set('ip', rsg.getIP());
+        queryParams.set('port', rsg.getPort());
+        queryParams.set('shell', rsg.getShell());
+        queryParams.set('encoding', rsg.getShell());
+
+        return `/${encodeURIComponent(name)}?${queryParams}`
+    },
+
+    withCustomValue: (value) => {
+        const queryParams = new URLSearchParams();
+        queryParams.set('value', value)
+        return `/raw?${queryParams}`
+    }
+}
---
+++ server_functions/raw.js
@@ -0,0 +1,60 @@
+const { rsgData } = require("../js/data.js");
+
+const insertParameters = function (command, params) {
+    // TODO: Extract the inlined JS from index.html into a new file,
+    // so the insertParameters + encoding logic can be reused
+    const encoder = (value) => value;
+
+    return command
+        .replace(encoder('{ip}'), encoder(params.ip))
+        .replace(encoder('{port}'), encoder(String(params.port)))
+        .replace(encoder('{shell}'), encoder(params.shell))
+}
+
+const generateCommand = function (event, _context) {
+    const { path, queryStringParameters } = event;
+
+    const requiredName = decodeURIComponent(path.substring(1));
+    const selectedItem = rsgData.reverseShellCommands.find(function ({ name }) {
+        return requiredName === name;
+    });
+
+    if (!selectedItem) {
+        return {
+            statusCode: 401,
+            body: `Command name '${requiredName}' not found`
+        }
+    }
+
+    const { command } = selectedItem;
+    const result = insertParameters(command, queryStringParameters);
+
+    return {
+        statusCode: 200,
+        body: result
+    }
+}
+
+const extractRawValue = function (event) {
+    const { queryStringParameters } = event;
+
+    return {
+        statusCode: 200,
+        body: queryStringParameters.value
+    }
+}
+
+exports.handler = async function (event, _context) {
+    const { queryStringParameters } = event;
+    const defaultHeaders = { headers: { 'Content-Type': "text/plain;charset=UTF-8" } };
+    if (queryStringParameters.value) {
+        return {
+            ...defaultHeaders,
+            ...extractRawValue(event)
+        }
+    }
+    return {
+        ...defaultHeaders,
+        ...generateCommand(event)
+    }
+}
